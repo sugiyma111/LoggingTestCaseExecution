3bf360f6404693e2bfeb9b2ecb3d70267310e852
sugiyma111-desktop <b1021169@fun.ac.jp>
2024-07-01 11:54:34 +0900

    change sort-library

--- sort-library/sortLibrary.py
+++ sort-library/sortLibrary.py
@@ -8,11 +8,14 @@ csv_dataids = 'dataids.csv'
 methods_df = pd.read_csv(csv_methods)
 dataids_df = pd.read_csv(csv_dataids)
 
+#ライブラリを読み込んでいる行を抜き出す
 dataids_df['Library'] = dataids_df['Attributes'].str.extract(r'owner=([^,]+)')
 dataids_df = dataids_df.dropna(subset=['Library'])
 
+#必要なラベルを取り出す
 library_usage_df = dataids_df[['ClassID', 'MethodID', 'Library']]
 
+#メソッドとライブラリのdfを左結合
 merged_df = pd.merge(methods_df, library_usage_df, on=['ClassID', 'MethodID'], how='left')
 
 method_library_usage = merged_df[['ClassName', 'MethodName', 'Library']].drop_duplicates().reset_index(drop=True)
