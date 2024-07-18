6f969346f5304507947b16d86bfd8c4d1165af0b
sugiyma111-desktop <b1021169@fun.ac.jp>
2024-07-18 11:33:49 +0900

    edit sortLibrary.py

--- sort-library/sortLibrary.py
+++ sort-library/sortLibrary.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import re
 
 #ファイルの読み込み
 methods_df = pd.read_csv('methods.csv')
 dataids_df = pd.read_csv('dataids.csv')
 log_df = pd.read_csv('log-00001.csv', header=None)
 
-#辞書の作成(key:メソッド名 value:メソッドIDのリスト)
-methodNameID_dict = {}
-current_method = None
+#リストの作成(MethodID, MethodName)
+mid_mname_list = []
+testmid_list = []
 
 for index, row in methods_df.iterrows():
     method_name = row['MethodName']
@@ -15,50 +16,68 @@ for index, row in methods_df.iterrows():
     
     #メソッド名がtestで始まるか
     if method_name.startswith('test'):
-        #新しいtestメソッドのときにMethodIDのリストを初期化
-        current_method = method_name
-        methodNameID_dict[current_method] = [method_id]
-    else:
-        #testメソッドでないときMethodIDを現在のメソッドのリストに追加
-        if current_method:
-            methodNameID_dict[current_method].append(method_id)
+        testmid_list.append(method_id)
+        work_list = []
+        work_list.append(method_id)
+        work_list.append(method_name)
+        mid_mname_list.append(work_list)
 
-print(methodNameID_dict)
+print(mid_mname_list)
 
 #リストの作成(methodID, Entry地点のdataID, Exit地点のdataID)
-methodIdLibrary_list = []
+mid_start_end = []
 
-dataids_df = dataids_df.dropna(subset=['Attributes'])
 for index, row in dataids_df.iterrows():
-    index = 0
+    if row['EventType'] == 'METHOD_NORMAL_EXIT':
+        if row['MethodID'] in testmid_list:
+            work_list.append(row['DataID'])
+            mid_start_end.append(work_list)
+
     if row['EventType'] == 'METHOD_ENTRY':
-        idList = []
-        method_id = row['MethodID']
-        idList.append(method_id)
-        idList.append(row['DataID'])
-        methodIdLibrary_list.append(idList)
-    if row['EventType'] == 'METHOD_EXCEPTIONAL_EXIT':
-        methodIdLibrary_list[method_id].append(row['DataID'])
-print(methodIdLibrary_list)
+        if row['MethodID'] in testmid_list:
+            work_list = []
+            work_list.append(row['MethodID'])
+            work_list.append(row['DataID'])
 
-midDid_list = []
-method_id = 0
-mode = 0
-for row in log_df[1]:
-    if mode == 0:
-        if row == methodIdLibrary_list[method_id][1]:
-            idList = []
-            idList.append(row)
+print(mid_start_end)
+
+#リストの作成(MethodID, [MethodID毎のDataIDリスト])
+mid_did_list = []
+
+for se in mid_start_end:
+    work_list = []
+    dataid_list = []
+    mode = 0
+    work_list.append(se[0])
+    
+    for index in log_df[1]:
+        if mode == 1:
+            if index == se[2]:
+                work_list.append(dataid_list)
+                mid_did_list.append(work_list)
+                break
+            dataid_list.append(index)
+        if index == se[1]:
             mode = 1
-    if mode == 1:
-        if row == methodIdLibrary_list[method_id][2]:
-            method_id = method_id + 1
-            mode = 0
-            print(idList)
-            midDid_list.append(idList)
-        else:
-            idList.append(row)
-print(midDid_list)
-        
 
+print(mid_did_list)
 
+#リストの作成(MethodID, [ライブラリのリスト])
+filtered_dataids_df = dataids_df[dataids_df['EventType'] == 'CALL']
+
+i = 0
+mid_lname_list = []
+for id in mid_did_list:
+    work_list = []
+    name_list = []
+    work_list.append(id[0])
+    for index, row in filtered_dataids_df.iterrows():
+        if row['DataID'] in id[1]:
+            match = re.search(r"owner=([\w/]+)", row['Attributes'])
+            library_name = match.group(1)
+            name_list.append(library_name)
+    name_list = list(set(name_list))
+    work_list.append(name_list)
+    mid_lname_list.append(work_list)
+
+print(mid_lname_list)
