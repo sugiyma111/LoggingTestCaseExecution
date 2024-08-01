b84297ab0b771db6a450690ba0e008184148ba1a
sugiyma111-desktop <b1021169@fun.ac.jp>
2024-08-01 12:20:16 +0900

    add script file

---
+++ script/script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# カレントディレクトリのパスを取得
+current_dir=$(pwd)
+
+# ログファイルのコピー先を保存するディレクトリを作成
+timestamp=$(date +"%Y%m%d%H%M%S")
+new_dir="$current_dir/$timestamp"
+mkdir $new_dir
+
+# 引数で選択したディレクトリ(dir2)に移動する
+if [ -z "$1" ]; then
+  echo "エラー: ディレクトリパスを引数として指定してください。"
+  exit 1
+fi
+
+dir2=$1
+if [ ! -d "$dir2" ]; then
+  echo "エラー: 指定されたディレクトリが存在しません。"
+  exit 1
+fi
+
+cd $dir2
+
+# 選択したディレクトリでテスト実行
+mvn -DargLine="-Xmx4g -Xms1024m -javaagent:../selogger-0.5.1.jar=format=omni,weaverlog=log.txt,output={time},weave=EXEC+CALL,e=org/junit,e=org/apache/maven,e=org/opentest4j" test
+
+# 降順にした際に一番最初のディレクトリ(dir3)に移動する
+echo "Available directories:"
+ls -d */
+dir3=$(ls -d */ | grep -E '^[0-9]{8}-[0-9]{9}/$' | sort -r | head -n 1)
+if [ -z "$dir3" ]; then
+  echo "エラー: 該当するディレクトリが見つかりません。"
+  exit 1
+fi
+
+echo "Navigating to directory: $dir3"
+cd $dir3
+
+# dir3のtxtファイルをcsvに変換して保存
+echo "Copying .txt files to $new_dir as .csv files"
+for file in *.txt; do
+  if [[ ! "$file" =~ ^'.*' ]]; then
+    cp "$file" "$new_dir/${file%.txt}.csv"
+    echo "Copied $file to $new_dir/${file%.txt}.csv"
+  else
+    echo "Skipping $file"
+  fi
+done
+
+# ライブラリ抽出のプログラムを実行
+cd $current_dir
+python3 sortLibrary.py $new_dir
---
+++ sort-library/sortLibrary2.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import re
+import glob
+
+#ファイルの読み込み
+methods_df = pd.read_csv('methods.csv')
+dataids_df = pd.read_csv('dataids.csv')
+
+# "log"で始まるすべてのファイルを読み込む
+log_files = sorted(glob.glob('log*.csv'))
+log_df_list = [pd.read_csv(log_file, header=None) for log_file in log_files]
+print(log_df_list)
+
+# リストの作成(MethodID, MethodName)
+mid_mname_list = []
+testmid_list = []
+
+for index, row in methods_df.iterrows():
+    method_name = row['MethodName']
+    method_id = row['MethodID']
+    
+    # メソッド名がtestで始まるか
+    if method_name.startswith('test'):
+        testmid_list.append(method_id)
+        work_list = []
+        work_list.append(method_id)
+        work_list.append(method_name)
+        mid_mname_list.append(work_list)
+
+print(mid_mname_list)
+
+# リストの作成(methodID, Entry地点のdataID, Exit地点のdataID)
+mid_start_end = []
+
+for index, row in dataids_df.iterrows():
+    if row['EventType'] == 'METHOD_NORMAL_EXIT':
+        if row['MethodID'] in testmid_list:
+            work_list.append(row['DataID'])
+            mid_start_end.append(work_list)
+
+    if row['EventType'] == 'METHOD_ENTRY':
+        if row['MethodID'] in testmid_list:
+            work_list = []
+            work_list.append(row['MethodID'])
+            work_list.append(row['DataID'])
+
+print(mid_start_end)
+
+# リストの作成(MethodID, [MethodID毎のDataIDリスト])
+mid_did_list = []
+
+for se in mid_start_end:
+    work_list = []
+    dataid_list = []
+    mode = 0
+    work_list.append(se[0])
+    
+    for log_df in log_df_list:
+        for index in log_df[1]:
+            if mode == 1:
+                if index == se[2]:
+                    work_list.append(dataid_list)
+                    mid_did_list.append(work_list)
+                    break
+                dataid_list.append(index)
+            if index == se[1]:
+                mode = 1
+
+print(mid_did_list)
+
+# リストの作成(MethodID, [ライブラリのリスト])
+filtered_dataids_df = dataids_df[dataids_df['EventType'] == 'CALL']
+
+i = 0
+mid_lname_list = []
+for id in mid_did_list:
+    work_list = []
+    name_list = []
+    work_list.append(id[0])
+    for index, row in filtered_dataids_df.iterrows():
+        if row['DataID'] in id[1]:
+            match = re.search(r"owner=([\w/]+)", row['Attributes'])
+            library_name = match.group(1)
+            name_list.append(library_name)
+    name_list = list(set(name_list))
+    work_list.append(name_list)
+    mid_lname_list.append(work_list)
+
+print(mid_lname_list)
